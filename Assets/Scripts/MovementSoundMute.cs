4c473343fd0a0622f8de35fd27dd8d95c21b45d1
esfdk <jakobmelnyk@hotmail.com>
2013-10-14 16:24:52 +0200

    First commit - base game

---
+++ Assets/Scripts/MovementSoundMute.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementSoundMute : MonoBehaviour {
+	
+	public AudioClip audioFile;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S)){
+			if(!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey (KeyCode.S))){
+				audio.clip = audioFile;
+				audio.Stop();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MovementSoundPlay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementSoundPlay : MonoBehaviour {
+	
+	public AudioClip audioFile;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S)){
+			audio.clip = audioFile;
+			audio.Play();
+		}
+	}
+}
