c57558281dfd47df1f08613aff452d271367e37b
esfdk <jakobmelnyk@hotmail.com>
2013-11-29 22:59:37 +0100

    Fixed endless world holyeee hsieeet

--- Assets/Scripts/EndlessWorld.cs
+++ Assets/Scripts/EndlessWorld.cs
@@ -11,22 +11,18 @@ public class EndlessWorld : MonoBehaviour {
 			case "WestTerrain": 
 				p = new Vector3(p.x + 1250, p.y, p.z);
 				transform.position = p;
-				Debug.Log("it worked - W");
 				break;
 			case "EastTerrain": 
 				p = new Vector3(p.x - 1250, p.y, p.z);
 				transform.position = p;
-				Debug.Log("it worked - E");
 				break;
 			case "NorthTerrain": 
 				p = new Vector3(p.x, p.y, p.z - 1250);
 				transform.position = p;
-				Debug.Log("it worked - N");
 				break;
 			case "SouthTerrain": 
 				p = new Vector3(p.x, p.y, p.z + 1250);
 				transform.position = p;
-				Debug.Log("it worked - S");
 				break;
 		}
 	}
